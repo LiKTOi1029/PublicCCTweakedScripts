370f539b56121ed9c0a6bb3ee9890572b7e226a7
LiKTOi1029 <testingboy5689@gmail.com>
2026-09-07 19:14:48 +0330

    lumber.lua adding tree coordinates input and option in Lumber.Init()

--- lumber.lua
+++ lumber.lua
@@ -2,7 +2,9 @@ local Defaults = require("defaults")
 local RCS = require(Defaults.Path.RCS)
 local Tablify = require(Defaults.Path.Tablify)
 local Lumber = {}
+Lumber.CoordinatesFormat = "^%-?%d+ %-?%d+ %-?%d+$"
 Lumber.Personal = {}
+Lumber.Personal.Trees = {}
 Lumber.Settings = {}
 Lumber.Settings.File = "LumberConfig"
 
@@ -17,16 +19,30 @@ function Lumber.Init()
   io.write(Defaults.AllDebuggingProtocols["FORMAT"], "-X = Backwards. -Y = Down. -Z = Left.\n")
   io.write(Defaults.AllDebuggingProtocols["FORMAT"], "X -> Space -> Y -> Space -> Z\n")
   io.write(Defaults.AllDebuggingProtocols["FORMAT"], "Number -> Space -> Number -> Space -> Number\n")
+  local Choice
   repeat
 	io.write(Defaults.AllDebuggingProtocols["ANSWER"])
-	local Choice = io.read()
-	local Success = Choice:find("^%-?%d+ %-?%d+ %-?%d+$")
+	Choice = io.read()
+	local Success = Choice:find(Lumber.CoordinatesFormat)
 	if not Success then 
 	  io.write(Defaults.AllDebuggingProtocols["FAILED"], "Invalid format\n")
 	  io.write(Defaults.AllDebuggingProtocols["FORMAT"], "Number -> Space -> Number -> Space -> Number\n")
 	end
   until Success
   Lumber.Personal.Chest = Tablify.Execute("NORMAL", Choice, " ")
+  io.write(Defaults.AllDebuggingProtocols["INFO"], "Please input every single tree's location.\n")
+  io.write(Defaults.AllDebuggingProtocols["INFO"], "Type 'done' when all coordinates have been written\n")
+  repeat
+	io.write(Defaults.AllDebuggingProtocols["ANSWER"])
+	Choice = io.read():upper()
+	local Success = Choice:find(Lumber.CoordinatesFormat)
+	if not Success and Choice ~= "EXIT" then
+	  io.write(Defaults.AllDebuggingProtocols["FAILED"], "Invalid format\n")
+	  io.write(Defaults.AllDebuggingProtocols["FORMAT"], "Number -> Space -> Number -> Space -> Number\n")
+	elseif Success then
+	  Lumber.Personal.Trees[#Lumber.Personal.Trees+1] = Tablify.Execute("NORMAL", Choice, " ")
+	end
+  until Choice == "EXIT"
 end
 
 Lumber.Init()
